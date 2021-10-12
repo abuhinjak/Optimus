78d4865f439d44760f76ec327133e25e4c8a7b74
abuhinjak <aleksandar.buhinjak@gmail.com>
2021-10-12 02:16:02 +0200

    Šesti commit - ispravak responsivness

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ function reveal(){
 
 // AOS
 AOS.init({
-  offset: 70, // offset (in px) from the original trigger point
+  offset: 80, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000 // values from 0 to 3000, with step 50ms
 });
